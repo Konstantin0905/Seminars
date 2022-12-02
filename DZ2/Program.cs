2f3e0096b98e953950daee074f89a4a47b24a8df
Konstantin <kos-petrov@mail.ru>
2022-12-02 16:49:11 +0300

    add DZ2

---
+++ DZ2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье чисто: ");
+int q = Convert.ToInt32(Console.ReadLine());
+
+int max = n;
+
+if (m > max)
+    max = m; 
+if (q > max)
+    max = q;
+Console.WriteLine($"максимальное число: {max}");
