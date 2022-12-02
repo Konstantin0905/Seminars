2cdb84752d5e5f05a03cfb94395acd81a799beb6
Konstantin <kos-petrov@mail.ru>
2022-12-02 16:26:08 +0300

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите вотрое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+    Console.WriteLine($"{n} больше {m}");
+else
+    Console.WriteLine($"{m} больше {n}");
