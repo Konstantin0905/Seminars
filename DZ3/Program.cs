d9ca8c86ec86058ef084e52dd3131547d54fd32c
Konstantin <kos-petrov@mail.ru>
2022-12-04 10:25:10 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+    Console.WriteLine("Ваше число четное!");
+else
+    Console.WriteLine("Ваше число нечетное! ");
