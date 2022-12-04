dbb34afbcf486d887a6862f9c2fa72dbc27d7bff
Konstantin <kos-petrov@mail.ru>
2022-12-04 15:17:58 +0300

    add DZ4

---
+++ DZ4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = (2); i <= n; i += 2)
+
+int i = 2;
+while (i <= n)
+{
+    Console.Write($"{i} ");
+i += 2;
+}
