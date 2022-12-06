80f70afc2653ca893769d0fb1db34c57cdd9dc82
Konstantin <kos-petrov@mail.ru>
2022-12-06 11:03:50 +0300

    add Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100;
+int n2 = n % 10;
+
+//Console.WriteLine(n2);
+//Console.WriteLine(n1);
+
+    Console.WriteLine(((n - n1*100) - n2)/10 );
