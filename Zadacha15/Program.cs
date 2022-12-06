698a11496131e8ea45d8b90b8e5dd289e62bc81f
Konstantin <kos-petrov@mail.ru>
2022-12-06 21:53:19 +0300

    add Zadacha15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 7)
+{
+    Console.Write("В неделе всего 7 дней!!\nВведите день недели:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n < 6)
+    Console.WriteLine("Будний день ;(");
+else
+    Console.WriteLine("Ура, ВЫХОДНОЙ!!");
