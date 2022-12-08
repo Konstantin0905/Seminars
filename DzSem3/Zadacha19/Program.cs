c4f3d19d5e06aaba49ee177d6df191b48f63ecaf
Konstantin <kos-petrov@mail.ru>
2022-12-08 23:43:11 +0300

    add DzSem3

---
+++ DzSem3/Zadacha19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? n =  Console.ReadLine();
+
+if (n[0] == n[4] && n[1] == n[3])
+    Console.WriteLine("Ура, ваше число - палиндромом!!");
+else 
+    Console.WriteLine("Ваше число - НЕ палиндромом ;(");
---
+++ DzSem3/ZadachaDop2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+Console.Write("Добро пожаловать на черничную ферму!! Сколько у вас будет кустов с ягодами:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] coll)
+{
+    int index = 0;
+    while (index < n)
+    {
+        Console.Write($"Количество ягод на {index + 1}-м кусте: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        coll[index] =  y;
+        index++;
+    }
+}
+int[] array = new int[n];
+
+FillArray(array);
+
+int index = 1;
+int max = 0;
+int best = 0;
+while (index < (n -1))
+{
+    if (array[index - 1] + array[index] + array[index + 1] > max)
+    {
+        max = (array[index - 1] + array[index] + array[index + 1]);
+        best = index;
+    }
+    index++;
+}
+
+Console.WriteLine($"Лучше всего собирать на {best + 1}-м кусте!! Вы соберете: {array[best - 1] + array[best] + array[best + 1]} ягод черники !!!");
