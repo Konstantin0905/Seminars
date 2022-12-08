8cad211ad7f935e821f5625e3cbff1dd5ed94b27
Konstantin <kos-petrov@mail.ru>
2022-12-08 14:06:40 +0300

    add ZadachaDop1

---
+++ ZadachaDop1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+
+int x = 0;
+int y = 0;
+int z = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+        
+        if (n > x)
+        {
+            x = n;
+            
+                if (y < x)
+                {
+                    z = y;
+                    y = x;
+                    x = z;
+                }
+                
+        }
+
+
+}
+Console.WriteLine($"{x}");
