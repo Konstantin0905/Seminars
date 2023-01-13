af283abc1a5864025c3aeb3696ef865f297addf3
Konstantin <kos-petrov@mail.ru>
2023-01-13 11:21:55 +0300

    add DzSem9

---
+++ DzSem9/Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int rec(int a, int b)
+{
+    if (b == a)
+        return a;
+    return rec(a, b - 1) + b;
+}
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(a, b));   
---
+++ DzSem9/Task68/Program.cs
@@ -0,0 +1,15 @@
+﻿int rec(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if  (n == 0)
+        return rec(m - 1, 1);
+    return rec(m - 1, rec (m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));   
